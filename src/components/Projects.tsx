6112f3a9f2da2e03da5df26ed20fdae84dca02dd
V-Sreshta <vaddesreshta133@gmail.com>
2025-12-12 15:24:28 +0530

    pr2

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects: React.FC = () => {
         <h2 className="text-3xl font-bold text-center mb-10 dark:text-white">Projects</h2>
         <div className="space-y-4 text-gray-700 dark:text-gray-300">
           <p><strong>Chronic Kidney Disease Detection</strong>Designed a CKD detection model using Random Forest Classifier in Python, identifying CKD from blood sample
-data</></p>
+data</p>
           <p><strong>Bank Loan System</strong> — HTML, CSS, JS | Oct 2023 - Nov 2023</p>
           <p><strong>Netflix Clone</strong> — HTML, CSS, JS | Aug 2023 - Sep 2023</p>
           {/* Add more projects */}
